829dea26ed2c96784556c1a9aa9c1648abcb143b
Franco Rebollo <francolucianorebollo@gmail.com>
2024-08-03 00:56:39 -0300

    Third commit, just testing something?

--- types.go
+++ types.go
@@ -1,6 +1,16 @@
 package main
 
+import "time"
+
 type UsersLogStruct struct {
-	userName     string
-	userPassword string
+	userName     string `json:userName`
+	userPassword string `json:userPassword`
+}
+
+type UsersSignUp struct {
+	userName         string    `json:userName`
+	userEmail        string    `json:userEmail`
+	userPassword     string    `json:userPassword`
+	userPersonalID   string    `json:userPersonalID`
+	userBirthdayDate time.Time `json:userBirthdayDate`
 }
